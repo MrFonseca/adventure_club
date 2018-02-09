c636d246b01de97d07339713ebbad6ba81d009a2
MrFonseca <fredidf@gmail.com>
2018-02-09 09:16:03 -0300

    Creates User Model
    - Installs gem bcrypt for secure user password;
    - Creates User model;
    - Implements user model test;
    - Sets up rspec tests;

---
+++ app/models/user.rb
@@ -0,0 +1,16 @@
+class User < ApplicationRecord
+  enum role: { member: 0, technical: 1, financial: 2, manager: 3 }
+  validates :role, presence: true
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true,
+                    format: { with: VALID_EMAIL_REGEX },
+                    uniqueness: true
+  before_validation :normalize_email
+  has_secure_password
+
+  private
+
+  def normalize_email
+    email.downcase!
+  end
+end
---
+++ spec/models/user_spec.rb
@@ -0,0 +1,48 @@
+RSpec.describe User, type: :model do
+  subject { described_class.new(FactoryBot.attributes_for(:user)) }
+
+  describe "validations" do
+    it "is valid with valid attributes" do
+      expect(subject).to be_valid
+    end
+
+    describe "#role" do
+      it "is not valid with empty role" do
+        subject.role = ""
+        expect(subject).to be_invalid
+        expect(subject.errors[:role]).to be_present
+      end
+    end
+
+    describe "#email" do
+      it "is not valid with empty email" do
+        subject.email = ""
+        expect(subject).to be_invalid
+        expect(subject.errors[:email]).to be_present
+      end
+
+      it "is not valid with invalid format" do
+        subject.email = "invalid_format"
+        expect(subject).to be_invalid
+        expect(subject.errors[:email]).to be_present
+      end
+
+      it "is not valid with matching email" do
+        FactoryBot.create(:user, email: "user@user.com")
+        subject.email = "user@user.com"
+        expect(subject).to be_invalid
+        expect(subject.errors[:email]).to be_present
+      end
+    end
+  end
+
+  describe "callbacks" do
+    context "before_validation" do
+      it "coverts e-mail to lowercase" do
+        subject.email = "USER@uSEr.com"
+        subject.save
+        expect(subject.email).to eq("user@user.com")
+      end
+    end
+  end
+end
